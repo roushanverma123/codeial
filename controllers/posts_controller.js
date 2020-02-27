4b52aabae7ae432d2cd8bb463f0ee6b0696695e1
roushanverma123 <roushanverma123@gmail.com>
2020-02-27 13:18:00 +0530

    added noty js and notifications using connect-flash library

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,12 +7,13 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
-    
+        req.flash('success','post published !');
         return res.redirect('back');
 
     }catch(err){
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error',err);
+        return res.redirect('back');
     }
   
 }
@@ -27,8 +28,12 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
+
+            req.flash('success', 'post and comments deleted');
+
             return res.redirect('back');
         }else{
+            req.flash('error', 'You cannot detete this post !');
             return res.redirect('back');
         }
 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -72,11 +72,12 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
-
+    req.flash('success', 'Logged out successfully');
     return res.redirect('/');
 }
